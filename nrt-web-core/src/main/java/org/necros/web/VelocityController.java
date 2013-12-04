fdeac821974e1b3d3a19b66f06f4e94f0cd29a4b
weiht <weihthchk@gmail.com>
2013-12-05 00:43:23 +0800

    Velocity controller implemented.

---
+++ nrt-web-core/src/main/java/org/necros/web/VelocityController.java
@@ -0,0 +1,16 @@
+package org.necros.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
+
+@Controller
+public class VelocityController {
+	@RequestMapping(value="/**")
+	public String doVelocity(HttpServletRequest request) {
+		String path = (String) request.getAttribute( HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE );
+		return path;
+	}
+}
